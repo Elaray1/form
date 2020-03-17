bb65c4b8c40fff0bab42cd6b4365bb02fbd108df
Egor <5642452@mail.ru>
2020-03-17 08:19:32 +0300

    refactor: change wifi networks api

--- api/routes/getWifiNetworks.js
+++ api/routes/getWifiNetworks.js
@@ -1,100 +1,41 @@
 const express = require('express');
 const router = express.Router();
-var iw = require('wireless-tools/iw');
+var wifi = require("node-wifi");
 
-let networksInfo;
+let avaibleWifiNetworks = [];
 
-iw.scan('wlan0', function(err, networks) {
-  console.log(networks)
-  networksInfo = networks;
+// Initialize wifi module
+// Absolutely necessary even to set interface to null
+wifi.init({
+  iface: null // network interface, choose a random wifi interface if set to null
 });
 
-iw.scan({ iface : 'wlan0', show_hidden : true }, function(err, networks) {
-  console.log(networks);
+// Scan networks
+wifi.scan(function(err, networks) {
+    avaibleWifiNetworks = networks;
+    /*
+        networks = [
+            {
+              ssid: '...',
+              bssid: '...',
+              mac: '...', // equals to bssid (for retrocompatibility)
+              channel: <number>,
+              frequency: <number>, // in MHz
+              signal_level: <number>, // in dB
+              quality: <number>, // same as signal level but in %
+              security: 'WPA WPA2' // format depending on locale for open networks in Windows
+              security_flags: '...' // encryption protocols (format currently depending of the OS)
+              mode: '...' // network mode like Infra (format currently depending of the OS)
+            },
+            ...
+        ];
+        */
 });
 
 router.get('/', function(req, res, next) {
-  res.send(networksInfo);
+  res.send(avaibleWifiNetworks);
 });
 
 
-var spawn = require('child_process').spawn;
-wifiNetworksRSSI(function(err, data, raw) {
-  if (!err) {
-    console.log(data); // formatted data with SSID, BSSID, RSSI
-    // console.log(raw); // raw output from netsh
-  } else {
-    console.log(err);
-  }
-});
-
-function wifiNetworksRSSI(fn) {
-  // prepare result string of data
-  var res = '';
-  // spawn netsh with required settings
-  var netsh = spawn('netsh', ['wlan', 'show', 'networks', 'mode=bssid']);
-
-  // get data and append to main result
-  netsh.stdout.on('data', function (data) {
-    res += data;
-  });
-
-  // if error occurs
-  netsh.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
-  });
-
-  // when done
-  netsh.on('close', function (code) {
-    if (code == 0) { // normal exit
-      // prepare array for formatted data
-      var networks = [ ];
-      // split response to blocks based on double new line
-      var raw = res.split('\r\n\r\n');
-
-      // iterate through each block
-      for(var i = 0; i < raw.length; ++i) {
-        // prepare object for data
-        var network = { };
-
-        // parse SSID
-        var match = raw[i].match(/^SSID [0-9]+ : (.+)/);
-        if (match && match.length == 2) {
-          network.ssid = match[1];
-        } else {
-          network.ssid = '';
-        }
-
-        // if SSID parsed
-        if (network.ssid) {
-          // parse BSSID
-          var match = raw[i].match(' +BSSID [0-9]+ +: (.+)');
-          if (match && match.length == 2) {
-            network.bssid = match[1];
-          } else {
-            network.bssid = '';
-          }
-
-          // parse RSSI (Signal Strength)
-          var match = raw[i].match(' +Signal +: ([0-9]+)%');
-          if (match && match.length == 2) {
-            network.rssi = parseInt(match[1]);
-          } else {
-            network.rssi = NaN;
-          }
-
-          // push to list of networks
-          networks.push(network);
-        }
-      }
-
-      // callback with networks and raw data
-      fn(null, networks, res);
-    } else {
-      // if exit was not normal, then throw error
-      fn(new Error(code));
-    }
-  });
-}
 
 module.exports = router;
